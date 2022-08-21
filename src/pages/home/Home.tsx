79e20aaa11866b3615e7a3fd7e7378e9ff19887a
Miller <farwes1180@gmail.com>
2022-08-22 08:51:33 +0900

    remove circleBtn in Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,14 +2,13 @@ import './home.scss';
 
 import TodoStateProvider from '../../context/TodoState';
 import Header from '../../components/header/Header';
-import CircleBtn from '../../components/circleBtn/CircleBtn';
 import List from '../../components/list/List';
 
 export function Home() {
   return (
     <TodoStateProvider>
       <section className='home container'>
-        <Header content={'✔️'} trailler={<CircleBtn content='?' />} />
+        <Header content={'✔️'} />
         <List />
       </section>
     </TodoStateProvider>
